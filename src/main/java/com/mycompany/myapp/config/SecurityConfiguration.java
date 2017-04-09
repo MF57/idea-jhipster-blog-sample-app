70117dc450cb60ef6119686d4181b8ae2f5c7888
MF57 <p-bochenek@outlook.com>
2017-04-09 13:27:13 +0200

    no endpoint

--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -101,7 +101,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
-            .antMatchers("/api/hello").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/account/reset_password/init").permitAll()
             .antMatchers("/api/account/reset_password/finish").permitAll()
--- src/main/java/com/mycompany/myapp/web/rest/UserResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/UserResource.java
@@ -75,10 +75,6 @@ public class UserResource {
     }
 
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
 
     /**
      * POST  /users  : Creates a new user.
